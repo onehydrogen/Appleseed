ca6dc8dd3e9e0a0bcd71126fdbeb0743183a66c7
onehydrogenatom <111794401+onehydrogenatom@users.noreply.github.com>
2025-02-21 11:51:04 -0600

    Add requirements.txt and fix logo path

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ load_dotenv()
 PAGE_SIZE = int(os.getenv('PAGE_SIZE', '10'))
 DEPLOY_KEY_PATH = os.getenv('DEPLOY_KEY_PATH')
 REPO_URL = os.getenv('REPO_URL', 'https://github.com/onehydrogen/Appleseed.git')
-CSV_PATH = 'C:/Users/bendw/Downloads/legislative_analysis_AR_2025_20250217_111047 copy.csv'  # Update this line
+CSV_PATH = 'C:/Users/bendw/Downloads/legislative_analysis_AR_2025_20250217_111047.csv'  # Update this line
 DEBUG_MODE = os.getenv('DEBUG_MODE', 'False').lower() == 'true'
 
 # Path configuration
@@ -540,7 +540,7 @@ navbar = dbc.NavbarSimple(
         dbc.NavItem(dbc.NavLink("Donate", href="https://www.arappleseed.org/donate", active=True)),
         dbc.NavItem(dbc.NavLink("About", href="https://www.arappleseed.org/", target="_blank")),
     ],
-    brand=html.Img(src="/assets/AR Appleseed square crop white on maroon 300x300.png", height="40px"),  # Updated to .png
+    brand=html.Img(src="/assets/AR_Appleseed_logo.png", height="40px"),  # Updated to .png
     brand_href="#",
     color="primary",
     dark=True,
